d27e5f98e7a0305dc5a7817c7e4e5d4713c2bed1
thomshaw92 <thomasbasilshaw@gmail.com>
2023-07-21 16:39:22 +1000

    Bugs in file selection

--- volume_curv_stats_extraction/extract_metrics.py
+++ volume_curv_stats_extraction/extract_metrics.py
@@ -14,76 +14,74 @@ def calculate_intensity(intensity_data, label_data, label):
     intensity = intensity_data[mask].mean()
     return intensity
 
-def calculate_metrics_for_dataset(dataset_path, dataset_name):
+def check_and_convert_dtype(label_data):
+    if label_data.dtype not in [np.int16, np.int32, np.int64]:
+        print(f"Warning: Label data is not of integer type. Current type: {label_data.dtype}. Converting to integer...")
+        label_data = label_data.astype(np.int32)
+    return label_data
+def get_intensity_filename(label_file, modality):
+    return label_file.replace("_labels", "")
+
+
+def process_file(file, root, dataset_name):
     metrics = []
+    
+    split_filename = file.split('_')
+    subject_id = split_filename[0].replace("sub-", "")
+    session_id = split_filename[1].replace("ses-", "")
+    label_file = os.path.join(root, file)
+
+    label_data = nib.load(label_file).get_fdata()
+    label_data = check_and_convert_dtype(label_data)
+
+    voxel_dims = nib.load(label_file).header.get_zooms()
+    voxel_volume = np.prod(voxel_dims)
+
+    for label in np.unique(label_data):
+        volume = calculate_volume(label_data, voxel_volume).get(label, 0)
+
+        for modality in ['T1w', 'T2w']:
+            intensity_file = get_intensity_filename(label_file, modality)
+            if not os.path.exists(intensity_file):
+                print(f"Warning: {modality} intensity file not found: {intensity_file}")
+                continue
+
+            intensity_data = nib.load(intensity_file).get_fdata()
+            intensity = calculate_intensity(intensity_data, label_data, label)
+
+            metrics.append({
+                'Dataset': dataset_name,
+                'Subject_ID': subject_id,
+                'Session_ID': session_id,
+                'Modality': modality,
+                'Label': label,
+                'Volume': volume,
+                'Intensity': intensity
+            })
+    return pd.DataFrame(metrics)  # return DataFrame
+
+def calculate_metrics_for_dataset(dataset_path, dataset_name):
+    df = pd.DataFrame()
     
     for root, dirs, files in os.walk(dataset_path):
         for file in files:
             if 'labels' in file:
-                subject_id = file.split('_')[1]
-                session_id = file.split('_')[2]
-                modality = file.split('_')[3].split('.')[0]
-                label_file = os.path.join(root, file)
-                
-                label_data = nib.load(label_file).get_fdata()
-                
                 print(f"Processing {file}")
-                print(f"Label data type: {label_data.dtype}")
-                unique_values, counts = np.unique(label_data, return_counts=True)
-                print(f"Unique values in label data: {unique_values}")
+                file_metrics = process_file(file, root, dataset_name)
+                df = pd.concat([df, file_metrics], ignore_index=True)
+    return df
 
-                if label_data.dtype != np.int32:
-                    print("Warning: Label data is not integer type. Converting to integer...")
-                    label_data = label_data.astype(np.int32)
-                    unique_values, counts = np.unique(label_data, return_counts=True)
-                    print(f"Unique values in label data after conversion: {unique_values}")
-                
-                voxel_dims = nib.load(label_file).header.get_zooms()
-                voxel_volume = np.prod(voxel_dims)
-                
-                for label in range(1, 6):
-                    if label not in unique_values:
-                        print(f"Warning: Label {label} not found in {file}")
-                        continue
-                    
-                    volume = calculate_volume(label_data, voxel_volume).get(label, 0)
-                    
-                    split_filename = label_file.split("_")
-                    split_filename[3] = 'T1w.nii.gz'
-                    intensity_file = "_".join(split_filename)
-                    if not os.path.exists(intensity_file):
-                        print(f"Warning: Intensity file not found: {intensity_file}")
-                        continue
-                    
-                    intensity_data = nib.load(intensity_file).get_fdata()
-                    intensity = calculate_intensity(intensity_data, label_data, label)
-                    
-                    metrics.append({
-                        'Dataset': dataset_name,
-                        'Subject_ID': subject_id,
-                        'Session_ID': session_id,
-                        'Modality': 'T1w' if dataset_name in ['Sydney', 'EATT'] else 'T2w',
-                        'Label': label,
-                        'Volume': volume,
-                        'Intensity': intensity
-                    })
-    return metrics
 
-belong_path = '/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/data/final_analysis_labels/BeLong'
+belong_path = '/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/data/final_analysis_labelled_data/BeLong'
 belong_metrics = calculate_metrics_for_dataset(belong_path, 'BeLong')
 
-sydney_path = '/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/data/final_analysis_labels/Sydney'
+sydney_path = '/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/data/final_analysis_labelled_data/Sydney'
 sydney_metrics = calculate_metrics_for_dataset(sydney_path, 'Sydney')
 
-eatt_path = '/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/data/final_analysis_labels/EATT'
+eatt_path = '/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/data/final_analysis_labelled_data/EATT'
 eatt_metrics = calculate_metrics_for_dataset(eatt_path, 'EATT')
 
-metrics = belong_metrics + sydney_metrics + eatt_metrics
+df = pd.concat([belong_metrics, sydney_metrics, eatt_metrics])
+df.sort_values(['Dataset', 'Subject_ID', 'Session_ID', 'Modality', 'Label'], inplace=True)
+df.to_csv('/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/code/TongueSegMND/volume_curv_stats_extraction/results.csv', index=False)
 
-# Only create DataFrame and sort if there are metrics data
-if metrics:
-    df = pd.DataFrame(metrics)
-    df.sort_values(['Dataset', 'Subject_ID', 'Session_ID', 'Modality', 'Label'], inplace=True)
-    df.to_csv('/winmounts/uqtshaw/data.cai.uq.edu.au/TONGUESEG-Q5346/code/TongueSegMND/volume_curv_stats_extraction/results.csv', index=False)
-else:
-    print("No metrics data to process.")
