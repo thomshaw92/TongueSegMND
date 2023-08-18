d3edafd9df3c3b6c5c30ce5c7fe2363b3a3a0712
thomshaw92 <thomasbasilshaw@gmail.com>
2023-08-18 16:04:45 +1000

    New subjnames

--- fastsurfer/fastsurfer_process_SLURM.sh
+++ fastsurfer/fastsurfer_process_SLURM.sh
@@ -1,20 +1,26 @@
 #!/bin/bash --login
-#SBATCH --partition=ai
+
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=24G
+#SBATCH --cpus-per-task=20
+#SBATCH --mem=32G
 #SBATCH --job-name=fastsurfer
-#SBATCH --time=8:00:00
+#SBATCH --time=4:00:00
+#SBATCH --partition=ai_collab
 #SBATCH --account=a_barth
 #SBATCH --gres=gpu:a100:1
-#SBATCH -o slurm.output
-#SBATCH -e slurm.error
+#SBATCH --output=/scratch/user/uqtshaw/data/FastSurferOutput/belong2fastsurfer_%A_%a.out 
+#SBATCH --error=/scratch/user/uqtshaw/data/FastSurferOutput/belong2fastsurfer_%A_%a.err 
+#SBATCH --array=1-40 ##this is the number of subjects in $dataset indexed from 1
 
+dataset="BeLong" #dataset name
+# Extract subjName based on the array index 
+subjName=$(sed -n "${SLURM_ARRAY_TASK_ID}p" /scratch/user/uqtshaw/code/TongueSegMND/fastsurfer/${dataset}_subjnames.csv) #path to subjnames
+ 
 # load necessary modules
-ml cuda/11.7.0
+module load cuda/11.7.0
 
 # Running the script that runs a single subject through fastsurfer
-srun fastsurfer_script.sh
+srun bash /scratch/user/uqtshaw/code/TongueSegMND/fastsurfer/fastsurfer_script.sh ${subjName} ${dataset}
 
 
--- fastsurfer/fastsurfer_script.sh
+++ fastsurfer/fastsurfer_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 ml cuda
 nvidia-smi
+subjName=$1
+dataset=$2
 
 singularity exec --nv \
                  -B /scratch/user/uqtshaw/data/:/data \
@@ -8,5 +10,12 @@ singularity exec --nv \
                  -B /scratch/user/uqtshaw/data:/fs_license \
                  /scratch/user/uqtshaw/sif_containers/fastsurfer-gpu.sif \
                  /fastsurfer/run_fastsurfer.sh \
-		--parallel --sd /output --sid sub-019_somatomnd --vox_size '0.75' --T1 /data/sub-019_ses-01_acq-UNIDEN_run-1_T1w.nii.gz \
+		--parallel --sd /output --sid ${subjName}_ses-02 --T1 /data/${dataset}/${subjName}_ses-02_T1w.nii.gz \
 		--fs_license /fs_license/license.txt
+
+#cd /scratch/user/uqtshaw/data/FastSurferOutput/${subjName}
+#zip -r extra_fastsurfer_output.zip tmp trash touch scripts 
+#rm -r tmp trash touch scripts
+#mv /scratch/user/uqtshaw/data/FastSurferOutput/${subjName} /QRISdata/Q5346/data/fastsurfer_output/
+
+        #--vox_size '0.75'
